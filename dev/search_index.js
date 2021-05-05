2246c270dfe107278a170fb7937808e8c11fcaf4
Documenter.jl <documenter@juliadocs.github.io>
2021-05-05 22:50:28 +0000

    build based on 31e7a34

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[]
+[{"location":"","page":"Introduction","title":"Introduction","text":"I wonder if master has something in the index file if it will do something. ","category":"page"}]
 }
