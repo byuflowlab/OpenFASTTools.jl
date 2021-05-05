ae9e617b780160b4a479005bc92e3e2a33c0d4c3
cardoza2 <adam@cardoza.one>
2021-05-04 19:21:38 -0600

    Preparing for hosting documentation.

---
+++ docs/make.jl
@@ -0,0 +1,30 @@
+using Documenter, OpenFASTsr
+
+makedocs(
+        format = Documenter.HTML(),
+        pages = [
+            "Introduction" => "index.md", 
+            "Quick Start" => "index.md"
+        ],
+        sitename="My Documentation",
+        authors = "Adam Cardoza <adam.cardoza.online@gmail.com>",
+        )
+
+# makedocs(
+#     modules = [CCBlade],
+#     format = Documenter.HTML(),
+#     pages = [
+#         "Intro" => "index.md",
+#         "Quick Start" => "tutorial.md",
+#         "Guided Examples" => "howto.md",
+#         "API Reference" => "reference.md",
+#         "Theory" => "theory.md"
+#     ],
+#     repo="https://github.com/byuflowlab/CCBlade.jl/blob/{commit}{path}#L{line}",
+#     sitename="CCBlade.jl",
+#     authors="Andrew Ning <aning@byu.edu>",
+# )
+
+deploydocs(
+    repo = "github.com/byuflowlab/OpenFASTsr.jl.git",
+)
